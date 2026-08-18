7d7ee37d76d586a62e701fe318a25c4650b7421a
Luiz Fernando <fernando.profiss.7@gmail.com>
2026-08-18 12:06:14 -0300

    Fix: add all missing __init__.py files

--- expression/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Saida do Optmus: voz e ponte com o HUD."""
-
-from expression.tts import SentenceBuffer, SpeechSynthesizer, TTSEngine
-
-__all__ = ["SentenceBuffer", "SpeechSynthesizer", "TTSEngine"]
--- reports/__init__.py
+++
@@ -1,5 +0,0 @@
-"""Relatorios do Optmus Core."""
-
-from reports.mensal import DadosRelatorio, ReportlabAusente, gerar_pdf, montar_dados
-
-__all__ = ["DadosRelatorio", "ReportlabAusente", "gerar_pdf", "montar_dados"]
